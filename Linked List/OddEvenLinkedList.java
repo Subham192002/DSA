070ba9deedb0c6899960e9d75978fe849cb13f1e
Subham <subham19200212345@gmail.com>
2025-12-28 22:01:06 +0530

    Linked List Odd Even Question added

---
+++ Linked List/OddEvenLinkedList.java
@@ -0,0 +1,57 @@
+public class OddEvenLinkedList {
+
+    // Definition for singly-linked list.
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {}
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode oddEvenList(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+        return head;
+    }
+
+    // Optional: test it
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1,
+                new ListNode(2,
+                        new ListNode(3,
+                                new ListNode(4,
+                                        new ListNode(5)))));
+
+        head = oddEvenList(head);
+
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+    }
+}
